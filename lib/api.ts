dbdfe85eb6a131bda56585f61f003740edcf6835
Guyo Abdub <guyo@Guyos-MacBook-Air.local>
2025-12-19 16:01:14 +0300

    docker

--- lib/api.ts
+++ lib/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000'
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://104.237.150.116:1111'
 
 export const api = axios.create({
   baseURL: API_URL,
